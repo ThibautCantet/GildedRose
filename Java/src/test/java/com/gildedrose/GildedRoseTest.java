53dd787d04ded7e53d84ff612a8c5ee22c1d1080
Thibaut Cantet <thibaut.cantet@gmail.com>
2021-04-19 20:13:35 +0200

    golden master

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,140 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static com.gildedrose.GildedRose.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void updateQuality_should_not_update_sulfuras_quality_nor_sellin() {
+        Item sulfuras = new Item(SULFURAS_HAND_OF_RAGNAROS, 3, 4);
+        Item[] items = new Item[] {sulfuras};
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertEquals(3, sulfuras.sellIn);
+        assertEquals(4, sulfuras.quality);
     }
 
+    @Nested
+    class OtherItem {
+        @Test
+        void updateQuality_should_increase_quality_and_decrease_sellin_when_quality_is_less_than_50() {
+            Item otherItem = new Item("other item", 3, 4);
+            Item[] items = new Item[]{otherItem};
+            GildedRose app = new GildedRose(items);
+
+            app.updateQuality();
+
+            assertEquals(2, otherItem.sellIn);
+            assertEquals(3, otherItem.quality);
+        }
+
+        @Test
+        void updateQuality_should_decrease_quality_by_2_and_decrease_sellin_when_sellin_is_less_than_0() {
+            Item otherItem = new Item("other item", -1, 4);
+            Item[] items = new Item[]{otherItem};
+            GildedRose app = new GildedRose(items);
+
+            app.updateQuality();
+
+            assertEquals(-2, otherItem.sellIn);
+            assertEquals(2, otherItem.quality);
+        }
+
+        @Test
+        void updateQuality_should_not_update_quality_and_decrease_sellin_when_quality_equal_0() {
+            Item otherItem = new Item("other item", -1, 0);
+            Item[] items = new Item[]{otherItem};
+            GildedRose app = new GildedRose(items);
+
+            app.updateQuality();
+
+            assertEquals(-2, otherItem.sellIn);
+            assertEquals(0, otherItem.quality);
+        }
+    }
+
+    @Nested
+    class AgedBrie {
+        @Test
+        void updateQuality_should_increase_quality_and_decrease_sellin_when_quality_is_less_than_50() {
+            Item agedBrie = new Item(AGED_BRIE, 3, 4);
+            Item[] items = new Item[] {agedBrie};
+            GildedRose app = new GildedRose(items);
+
+            app.updateQuality();
+
+            assertEquals(agedBrie.sellIn, 2);
+            assertEquals(agedBrie.quality, 5);
+        }
+
+        @Test
+        void updateQuality_should_increase_quality_and_decrease_sellin_when_sellin_is_less_than_0() {
+            Item agedBrie = new Item(AGED_BRIE, -1, 4);
+            Item[] items = new Item[] {agedBrie};
+            GildedRose app = new GildedRose(items);
+
+            app.updateQuality();
+
+            assertEquals(-2, agedBrie.sellIn);
+            assertEquals(6, agedBrie.quality);
+        }
+
+        @Test
+        void updateQuality_should_not_increase_quality_and_decrease_sellin_when_quality_equal_50() {
+            Item agedBrie = new Item(AGED_BRIE, 3, 50);
+            Item[] items = new Item[] {agedBrie};
+            GildedRose app = new GildedRose(items);
+
+            app.updateQuality();
+
+            assertEquals(agedBrie.sellIn, 2);
+            assertEquals(agedBrie.quality, 50);
+        }
+    }
+
+
+    @Nested
+    class Backstage {
+        @Test
+        void updateQuality_should_increase_quality_by_2_and_decrement_sellin_when_sellin_between_10_and_5() {
+            Item backStage = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 10, 4);
+            Item[] items = new Item[]{backStage};
+            GildedRose app = new GildedRose(items);
+
+            app.updateQuality();
+
+            assertEquals(backStage.sellIn, 9);
+            assertEquals(backStage.quality, 6);
+        }
+
+        @Test
+        void updateQuality_should_increase_quality_by_1_and_decrement_sellin_when_sellin_less_than_5() {
+            Item backStage = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 5, 4);
+            Item[] items = new Item[]{backStage};
+            GildedRose app = new GildedRose(items);
+
+            app.updateQuality();
+
+            assertEquals(backStage.sellIn, 4);
+            assertEquals(backStage.quality, 7);
+        }
+
+        @Test
+        void updateQuality_should_update_quality_and_sellin_to_zero_when_sellin_is_zero() {
+            Item backStage = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 0, 4);
+            Item[] items = new Item[]{backStage};
+            GildedRose app = new GildedRose(items);
+
+            app.updateQuality();
+
+            assertEquals(-1, backStage.sellIn);
+            assertEquals(0, backStage.quality);
+        }
+    }
 }
