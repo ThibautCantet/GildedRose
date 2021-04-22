9624ebba8687b8a1e02addcb0e6877f20bb90c52
Thibaut Cantet <thibaut.cantet@NX-FR-LT324.local>
2021-04-22 14:58:51 +0200

    fizzbuzz branch

---
+++ Java/src/test/java/fizzbuzz/FizzbuzzTestTest.java
@@ -0,0 +1,13 @@
+package fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FizzbuzzTestTest {
+    @Test
+    void name() {
+
+        assertEquals(4, 4);
+    }
+}
